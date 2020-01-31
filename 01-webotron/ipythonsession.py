d8f17ebc045cf9f7875323a9857caa36b8da2d61
craquiest <laminegaye@me.com>
2020-01-31 11:43:27 +0000

    Setup project directory for webotron

--- 01-webotron/ipythonsession.py
+++ 01-webotron/ipythonsession.py
@@ -6,4 +6,4 @@ s3 = session.resource('s3')
 # new_bucket = s3.create_bucket(Bucket='aws-python-automation-alg', CreateBucketConfiguration={
 #         'LocationConstraint': 'eu-west-2'})
 # for bucket in s3.buckets.all(): print(bucket)
-# ec2_client = session.client('ec2')
+ec2_client = session.client('ec2')
