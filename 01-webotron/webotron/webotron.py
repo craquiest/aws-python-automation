680bfd0b5d45cf8ca54cbe0679453d121a10ad9a
Lamine Gaye <laminegaye@me.com>
2020-02-05 13:38:09 +0000

    Use click to organize webotron script

---
+++ 01-webotron/webotron/webotron.py
@@ -0,0 +1,32 @@
+import boto3
+# import sys
+import click
+
+session = boto3.Session(profile_name='craquiest')
+s3 = session.resource('s3')
+
+
+@click.group()
+def cli():
+    "Webotrom deploys websites to AWS"
+    pass
+
+
+@cli.command('list-buckets')
+def list_buckets():
+    "List all s3 buckets"
+    for bucket in s3.buckets.all():
+        print(bucket)
+
+
+@cli.command('list-bucket-objects')
+@click.argument('bucket')
+def list_bucket_objects(bucket):
+    """List objects in an s3 bucket"""
+    for obj in s3.Bucket(bucket).objects.all():
+        print(obj)
+
+
+if __name__ == '__main__':
+    # print(sys.argv)
+    cli()
--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Repository for Automating AWS with Python. ACG course
 
 Webotron is a script that will sync a local directory to an S3 bucket, 
 and optionally configyre Route53 and CloudFront as well. 
+
+### Features
+
+Webotron currently has the following features:
+
+- List buckets
+- List contents of a bucket
