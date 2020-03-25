a867e5fa7a17c3451f3a206631bd54f5be5d72dd
Lamine Gaye <laminegaye@me.com>
2020-03-25 20:01:35 +0000

    Save data from get_label_detection to dynamodb

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -24,9 +24,9 @@ def start_label_detection(bucket, key):
                     'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
                     }
                 )
-    print(response)
 
     return
+
 
 def get_video_labels(job_id):
     rekognition_client = boto3.client('rekognition')
@@ -48,9 +48,35 @@ def get_video_labels(job_id):
     return response
 
 
-def put_labels_in_db(data, video_name, video_bucket):
+def make_item(data):
+    if isinstance(data, dict):
+        return { k: make_item(v) for k, v in data.items() }
 
-    pass
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
+
+def put_labels_in_db(data, video_name, video_bucket):
+    del data['ResponseMetadata'] # save space by deleting unneeded
+    del data['JobStatus']
+
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+    videos_table.put_item(Item=data)
+
+    return
+
 
 #! Lambda Events
 
@@ -67,7 +93,6 @@ def start_processing_video(event, context):
 
 
 def handle_label_detection(event, context):
-
     for record in event['Records']:
         message = json.loads(record['Sns']['Message'])
         job_id = message['JobId']
@@ -75,7 +100,6 @@ def handle_label_detection(event, context):
         s3_bucket = message['Video']['S3Bucket']
 
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
